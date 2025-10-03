97f798e4352ff9c69085025e2259e6feb1006870
victoria-ogega <victoriaogega@gmail.com>
2025-10-03 13:33:57 +0300

    feat:added project setuo

---
+++ iddirnet-documentation/src/app/documentation/page.tsx
@@ -0,0 +1,34 @@
+export default function Documentation() {
+    return (
+        <div>
+            <div>
+                <h1>Why Iddirnet?</h1>
+            
+                <p>We bring communities together.IddirNet makes it easy to join, support, and connect with others, sharing life's joys
+                    and easing burdens all in one trusted space. We are  dedicated to strengthening the values of trust, transparency, and
+                     solidarity within Ethiopian Iddirs.</p>
+            </div>
+
+            <div>
+                <h1>Project Overview</h1>
+                <p></p>
+
+            </div>
+            <div>
+                <h1>Code structure</h1>
+            </div>
+
+            <div>
+                <h1>Features</h1>
+            </div>
+
+            <div>
+                
+            </div>
+
+
+            
+        </div>
+    )
+
+}
---
+++ iddirnet-documentation/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Documentation from "./documentation/page";
+export default function Home() {
+  return (
+    <div className="">
+ <Documentation/>
+      
+    </div>
+  );
+}
